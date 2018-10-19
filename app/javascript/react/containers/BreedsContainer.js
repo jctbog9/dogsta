fedc0e344f75144864a9da3c7ec3040cd5406a6d
John Trocchi <jctbog9@aol.com>
2018-10-19 19:09:34 -0400

    add initial breeds index and show api

--- app/javascript/react/containers/BreedsContainer.js
+++ app/javascript/react/containers/BreedsContainer.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import BreedBox from '../components/BreedBox';
+
 class BreedsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      breeds: []
     };
+  }
+
+  componentDidMount() {
+    // the fetch will go here and get the dog info from an API endpoint yet to be defined
   }
 
   render() {
@@ -12,6 +19,7 @@ class BreedsContainer extends Component {
     return(
       <div>
         <p>"Breeds Container (All Breeds)"</p>
+        <BreedBox />
       </div>
     );
   }
