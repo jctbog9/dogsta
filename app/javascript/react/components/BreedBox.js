a17a8b615dc7c6c688dc01250b9ea3f4aa5a935c
John Trocchi <41226602+jctbog9@users.noreply.github.com>
2018-10-20 12:02:13 -0400

    Update BreedBox.js

--- app/javascript/react/components/BreedBox.js
+++ app/javascript/react/components/BreedBox.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// might want to make this a container to maintain and change state.
-// This file will pull a list of all the breeds, decide if they are not in the users favorites, and display them one by one, changing based on which breeds the user has seen and which views the user has favorited. What is left it what the logic will choose from to display
-// possibly having a few different arrays in state to handle the information based on user favorites, already displayed to user in this session, and hasn't favorited or seen.
-
 const BreedBox = props => {
   return(
     <p>this is the breed box</p>
